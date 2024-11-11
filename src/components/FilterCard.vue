194f101975834a3328372ceececab84c256b4c52
Lorena Cerbino <lorenamcerbino@gmail.com>
2024-11-11 18:45:40 -0300

    feat: autocomplete filter on FilterCard

--- src/components/FilterCard.vue
+++ src/components/FilterCard.vue
@@ -14,12 +14,15 @@
                 stack-label
                 input-class="q-py-none"
                 use-input
+                hide-selected
+                fill-input
                 input-debounce="0"
-                :options="options"
+                :options="filteredOptions"
                 hide-dropdown-icon
                 option-value="value"
                 option-label="label"
                 @update:model-value="updateValue"
+                @filter="filterFn"
             />
 		</q-card-section>
         <q-card-actions align="right">
@@ -51,14 +54,16 @@
     function updateValue(newValue: string | number) {
         emit('update:place', newValue);
     }
-	// const filterFn = (val: string | number, update: (callback: () => void) => void) => {
-    //     update(() => {
-    //         const needle = String(val).toLowerCase()
-    //         options.value = options.filter((v) =>
-    //             String(v.value).toLowerCase().includes(needle)
-    //         )
-    //     })
-    // }
+
+    const filteredOptions = ref(props.options)
+	const filterFn = (val: string | number, update: (callback: () => void) => void) => {
+        update(() => {
+            const needle = String(val).toLowerCase()
+            filteredOptions.value = props.options.filter((v: Option) =>
+                String(v.label).toLowerCase().includes(needle)
+            )
+        })
+    }
 </script>
 
 <style lang="scss">
