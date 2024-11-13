51819906978975e088b7a039053b97a0af613df5
Lorena Cerbino <lorenamcerbino@gmail.com>
2024-11-13 13:13:00 -0300

    feat: show more action

--- src/components/HotelDetails.vue
+++ src/components/HotelDetails.vue
@@ -14,26 +14,29 @@
 
     <image-carousel :images="hotel?.images" width="100%" height="300px" style="border-radius: 8px;" showCounter />
     
-    <div class="text-h6 text-grey-8 text-weight-regular q-pt-md q-pb-sm">Facilidades do hotel</div>
-    <q-separator color="grey" />
-    <div class="row items-center q-gutter-sm justify-between q-py-lg q-gutter-sm">
-        <div v-for="amenity in getAmenities(hotel)" :key="amenity.key">
-            <div v-if="hasAmenity(amenity.key, hotel)" class="row no-wrap items-center q-gutter-xs">
-                <q-icon :name="amenity.icon" color="grey" />
-                <div class="text-body2 text-grey text-weight-light">{{ amenity?.label }}</div>
+    <div v-if="hotel?.amenities?.length > 0">
+        <div class="text-h6 text-grey-8 text-weight-regular q-pt-md q-pb-sm" >Facilidades do hotel</div>
+        <q-separator color="grey" />
+        <div class="row items-center q-gutter-sm justify-between q-py-lg q-gutter-md" >
+            <div v-for="amenity in amenitiesToShow()" :key="amenity.key">
+                <div v-if="hasAmenity(amenity.key, hotel)" class="row no-wrap items-center q-gutter-xs">
+                    <q-icon :name="amenity.icon" color="grey" />
+                    <div class="text-body2 text-grey text-weight-light">{{ amenity?.label }}</div>
+                </div>
+            </div>
+            <div v-if="hotel?.amenities?.length > 3" class="row justify-center" style="width: 100%;">
+                <q-btn unelevated rounded outline style="color: #009EFB;" padding="sm xl"  :label="showMore ? 'Mostrar menos facilidades' : 'Mostrar todas as facilidades'" no-caps size="md" :loading="false" :disable="false" @click="handleClick" />
             </div>
         </div>
-        <div class="row justify-center" style="width: 100%;">
-            <q-btn unelevated rounded outline style="color: #009EFB;" padding="sm xl"  label="Mostrar todas as facilidades" no-caps size="md" :loading="false" :disable="false" @click="handleClick" />
-        </div>
+        <q-separator color="grey" />
     </div>
-    <q-separator color="grey" />
 
     <div class="text-h6 text-grey-8 text-weight-regular q-py-sm">Conheça um pouco mais</div>
     <div class="text-body2 text-grey text-weight-regular">{{ hotel?.description }}</div>
 </template>
 
 <script setup lang="ts">
+    import { ref } from 'vue'
 	import { Hotel } from './models';
 	import ImageCarousel from './ImageCarousel.vue'
 
@@ -50,8 +53,15 @@
 
 	const props = defineProps<Props>();
 
+    const showMore = ref(false)
+    const amenitiesToShow = () => {
+        return showMore.value
+            ? getAmenities(props.hotel)
+            : getAmenities(props.hotel).slice(0, 3)
+    }
+    
 	const handleClick = () => {
-		props.handleHotelSelect(props.hotel)
+		showMore.value = !showMore.value
 	}
 	
 </script>
